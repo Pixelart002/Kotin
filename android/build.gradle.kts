725afd66880ee6c8241bacce8f56bd0aad33e32c
kyro <9013ms@gmail.com>
2026-03-02 09:44:42 +0530

    update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,27 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
+// settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    }
 }
-
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url = uri("https://repo.maven.apache.org/maven2")
-        }
-    }
-}
+rootProject.name = "upi alert"
+include(":android")   // ← Make sure this matches your actual module name
